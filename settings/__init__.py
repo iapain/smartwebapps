997cba0d7c82e69bf4e76864256f16f05365eca1
Deepak Thukral <deepak.thukral@fagbokforlaget.no>
2011-06-02 12:11:10 +0200

    added project skeleton

---
+++ settings/__init__.py
@@ -0,0 +1,139 @@
+# Django settings for webapp project.
+import os
+import sys
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+#dummy to bypass translations
+gettext = lambda s: s
+
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '../')
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
+
+ADMINS = (
+    ('Deepak Thukral', 'iapain@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+# Database settings will be override by local_settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, 'dev.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Oslo'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+LANGUAGES = (
+    ('no', gettext('Norwegian')),
+    ('en', gettext('English')),
+)
+
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/static/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'lj^3!_!wul0&jbt9^$j&_eq!h#2j91827$4ivyp^-+2opp)3i*'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "templates"),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.markup',
+    'apps.djangoratings',
+    'apps.movie',
+    'apps.news',
+    'apps.emoter',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.contrib.messages.context_processors.messages",
+    "core.context_processors.common",
+)
+
+# Registration prefs
+ACCOUNT_ACTIVATION_DAYS = 7
+
+# App name
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/dashboard'
+APP_NAME = 'EP2011 Demo'
+AUTH_PROFILE_MODULE = 'account.UserProfile'
+
+# Load local environment.
+try:
+    from local_settings import *
+except ImportError:
+    pass
